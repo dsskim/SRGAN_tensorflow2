8a2aa0afc0241d55fb8e5e9783b26f2a933f6a98
dss.kim <dss.kim@lge.com>
2020-01-16 18:04:51 +0900

    add content_loss

--- model.py
+++ model.py
@@ -49,11 +49,11 @@ def Generator(size=None, channels=3, res_block_repeat=16, zoom=4, name='generato
     x = BatchNormalization()(x)
     x = tf.add(x, skip_layer)
 
-    for _ in range (math.sqrt(zoom)):
-        x = Conv2DTranspose()
+    for _ in range(int(math.sqrt(zoom))):
+        x = Conv2DTranspose(filters=64, kernel_size=3, strides=2, padding='same', use_bias=False, kernel_regularizer=_regularizer)(x)
         x = PReLU(shared_axes=[1, 2])(x)
 
-    out = Conv2D(filters=64, kernel_size=9, strides=1, padding='same', use_bias=False, kernel_regularizer=_regularizer)(x)
+    out = Conv2D(filters=3, kernel_size=9, strides=1, padding='same', use_bias=False, kernel_regularizer=_regularizer, activation='tanh')(x)
 
     return Model(inputs, out, name=name)
 
@@ -87,3 +87,17 @@ def Discriminator(size=None, channels=3, name='discriminator'):
     out = Dense(1, use_bias=False, kernel_regularizer=_regularizer, activation='sigmoid')(x)
 
     return Model(inputs, out, name=name)
+
+
+def Content_Net(size=None, channels=3, i=5, j=4):
+    vgg19 = VGG19(weights='imagenet', include_top=False, input_shape=(size, size, channels))
+    block_name = 'block{}_conv{}'.format(i, j)
+
+    vgg19.trainable = False
+    # Make trainable as False
+    for l in vgg19.layers:
+        l.trainable = False
+    model = Model(inputs=vgg19.input, outputs=vgg19.get_layer(block_name).output)
+    model.trainable = False
+
+    return model
--- train.py
+++ train.py
@@ -1,22 +1,85 @@
 import os
 import argparse
+import time
 import tensorflow as tf
-from model import Generator, Discriminator
+from model import Generator, Discriminator, Content_Net
+
 
 def get_parser():
     parser = argparse.ArgumentParser(description='parameters to train net')
     parser.add_argument('--max_epoch', type=int, help='epoch to train the network', default=10)
-    parser.add_argument('--lr_image_size', help='the image size', default=28)
-    parser.add_argument('--hr_image_size', help='the image size', default=28)
-    parser.add_argument('--initial_learning_rate', type=float, help='Initial learning rate', default=1e-1)
-    parser.add_argument('--train_batch_size', type=int, help='batch size to train network', default=128)
+    parser.add_argument('--lr_image_size', help='the image size', default=24)
+    parser.add_argument('--hr_image_size', help='the image size', default=96)
 
     args = parser.parse_args()
     return args
 
+
+def discriminator_loss(real_output, fake_output):
+    cross_entropy = tf.keras.losses.BinaryCrossentropy()
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+
+def generator_loss(fake_output):
+    cross_entropy = tf.keras.losses.BinaryCrossentropy()
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+
+def content_loss(content_model, hr, sr):
+    # real_output, fake_output range: -1 ~ 1
+
+    mse = tf.keras.losses.MeanSquaredError()
+    hr_feature = content_model.predict(hr)
+    sr_feature = content_model.predict(sr)
+
+    return mse(hr_feature, sr_feature)
+
+
 def main(args):
     gene = Generator(args.lr_image_size)
+    tf.keras.utils.plot_model(gene, to_file='generator.png', show_shapes=True, show_layer_names=True, expand_nested=True)
     disc = Discriminator(args.hr_image_size)
+    tf.keras.utils.plot_model(disc, to_file='discriminator.png', show_shapes=True, show_layer_names=True, expand_nested=True)
+    content_model = Content_Net(args.hr_image_size)
+    tf.keras.utils.plot_model(content_model, to_file='content.png', show_shapes=True, show_layer_names=True, expand_nested=True)
+
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+    @tf.function
+    def train_step(lr, hr, generator, discriminator, content):
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            sr = generator(lr, training=True)
+
+            hr_output = discriminator(hr, training=True)
+            sr_output = discriminator(sr, training=True)
+
+            gen_loss = generator_loss(sr_output)
+            cont_loss = content_loss(content, hr, sr)
+            perceptual_loss = cont_loss + 1e-3 * gen_loss
+
+            disc_loss = discriminator_loss(hr_output, sr_output)
+
+        gradients_of_generator = gen_tape.gradient(perceptual_loss, generator.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+        generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+        discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+        return perceptual_loss, disc_loss
+    #
+    # for epoch in range(args.max_epoch):
+    #     start = time.time()
+    #
+    #     for image_batch in dataset:
+    #         gen_loss, disc_loss = train_step(image_batch, gene, disc, content_model)
+    #
+    #     # print (' 에포크 {} 에서 걸린 시간은 {} 초 입니다'.format(epoch +1, time.time()-start))
+    #     print('gen_loss {}, disc_loss {}, Time for epoch {} is {} sec'.format(gen_loss, disc_loss, epoch + 1,
+    #                                                                           time.time() - start))
 
 if __name__ == '__main__':
     # Setting GPUs
