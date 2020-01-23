50b50c72347272aa15c8c0c8f0b8b696e0b85fb8
dss.kim <dss.kim@lge.com>
2020-01-23 16:16:01 +0900

    add dataset class

--- train.py
+++ train.py
@@ -1,13 +1,18 @@
 import os
 import argparse
 import time
+import cv2
+import numpy as np
 import tensorflow as tf
+from tqdm import tqdm
+
 from model import Generator, Discriminator, Content_Net
+from dataset import DIV2K
 
 
 def get_parser():
     parser = argparse.ArgumentParser(description='parameters to train net')
-    parser.add_argument('--max_epoch', type=int, help='epoch to train the network', default=10)
+    parser.add_argument('--max_epoch', type=int, help='epoch to train the network', default=100)
     parser.add_argument('--lr_image_size', help='the image size', default=24)
     parser.add_argument('--hr_image_size', help='the image size', default=96)
 
@@ -32,13 +37,44 @@ def content_loss(content_model, hr, sr):
     # real_output, fake_output range: -1 ~ 1
 
     mse = tf.keras.losses.MeanSquaredError()
-    hr_feature = content_model.predict(hr)
-    sr_feature = content_model.predict(sr)
+    hr_feature = content_model(hr)
+    sr_feature = content_model(sr)
 
     return mse(hr_feature, sr_feature)
 
 
+def generate_and_save_images(model, epoch, input):
+    import matplotlib.pyplot as plt
+
+    predictions = model(input, training=False)
+
+    fig, axs = plt.subplots(4, 4)
+    for i, ax in enumerate(axs.flat):
+        ax.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+
+
 def main(args):
+
+    # Dataset
+    train_loader = DIV2K(scale=4,  # 2, 3, 4 or 8
+                         downgrade='unknown',  # 'bicubic', 'unknown', 'mild' or 'difficult'
+                         subset='train')  # Training dataset are images 001 - 800
+    valid_loader = DIV2K(scale=4,  # 2, 3, 4 or 8
+                         downgrade='unknown',  # 'bicubic', 'unknown', 'mild' or 'difficult'
+                         subset='valid')  # Validation dataset are images 801 - 900
+
+    # Create a tf.data.Dataset
+    train_ds = train_loader.dataset(batch_size=16,
+                                    random_transform=True,
+                                    repeat_count=1)
+    # Create a tf.data.Dataset
+    valid_ds = valid_loader.dataset(batch_size=1,  # use batch size of 1 as DIV2K images have different size
+                                    random_transform=False,  # use DIV2K images in original size
+                                    repeat_count=1)  # 1 epoch
+
     gene = Generator(args.lr_image_size)
     tf.keras.utils.plot_model(gene, to_file='generator.png', show_shapes=True, show_layer_names=True, expand_nested=True)
     disc = Discriminator(args.hr_image_size)
@@ -70,16 +106,25 @@ def main(args):
         discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
         return perceptual_loss, disc_loss
-    #
-    # for epoch in range(args.max_epoch):
-    #     start = time.time()
-    #
-    #     for image_batch in dataset:
-    #         gen_loss, disc_loss = train_step(image_batch, gene, disc, content_model)
-    #
-    #     # print (' 에포크 {} 에서 걸린 시간은 {} 초 입니다'.format(epoch +1, time.time()-start))
-    #     print('gen_loss {}, disc_loss {}, Time for epoch {} is {} sec'.format(gen_loss, disc_loss, epoch + 1,
-    #                                                                           time.time() - start))
+
+    for epoch in range(args.max_epoch):
+        start = time.time()
+
+        for lr, hr in train_ds:
+            # pre-processing
+            lr = tf.cast(lr, tf.float32)
+            hr = tf.cast(hr, tf.float32)
+            lr = lr / 127.5 - 1
+            hr = hr / 127.5 - 1
+
+            gen_loss, disc_loss = train_step(lr, hr, gene, disc, content_model)
+
+        print('epoch:{}/{} gen_loss {}, disc_loss {}, Time for epoch {} is {} sec'.format(epoch,
+                                                                                          args.max_epoch,
+                                                                                          gen_loss,
+                                                                                          disc_loss,
+                                                                                          epoch + 1,
+                                                                                          time.time() - start))
 
 if __name__ == '__main__':
     # Setting GPUs
